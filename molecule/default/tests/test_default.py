37594714abf68a31b10b552084c8f04f5cffaeb5
Matt DePorter <nfaction@gmail.com>
2019-09-19 00:05:00 -0700

    Add tests for RHEL systems

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,5 +10,13 @@ def test_ddclient_config(host):
     f = host.file('/etc/ddclient.conf')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+    os_distribution = host.system_info.distribution
+    redhat_systems = ['fedora', 'centos', 'redhat']
+
+    if os_distribution in redhat_systems:
+        assert f.user == 'ddclient'
+        assert f.group == 'ddclient'
+    else:
+        assert f.user == 'root'
+        assert f.group == 'root'
