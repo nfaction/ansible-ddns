5283a6f3df79c314dac1e87e42146295f4ccc9dd
Matt DePorter <nfaction@gmail.com>
2019-09-18 23:31:40 -0700

    Fix variable locations and enable web usage for google domains

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,8 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_ddclient_config(host):
+    f = host.file('/etc/ddclient.conf')
 
     assert f.exists
     assert f.user == 'root'
